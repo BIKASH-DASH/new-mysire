70e9e07c984a79dac8ee3500ad8e04879f08ed08
vikash dash <vikashdash004@gmail.com>
2016-10-04 17:25:48 +0530

    databases added

--- settings.py
+++ settings.py
@@ -13,9 +13,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'dashapp',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'password',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'USER': 'vikash007',                      # Not used with sqlite3.
+        'PASSWORD': 'vikash@123',                  # Not used with sqlite3.
+        'HOST': 'vikash007.mysql.pythonanywhere-services.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
